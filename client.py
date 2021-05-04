7404b8f1c853b453150380a405780608d9140533
Kun Hwi Ko <kokunhwi@gmail.com>
2021-05-04 04:39:39 -0400

    remove print statements used for debugging purposes

--- client.py
+++ client.py
@@ -55,7 +55,6 @@ class Packet:
     # decode packet received from client 
     def decode_to_packet(self, encoded_string): 
         decoding = encoded_string.split('<NEXT;>')
-        print("decoding: " + str(len(decoding)))
         if decoding[0] == 'stop':
             self.msg_type = decoding[0]
         else:
@@ -86,7 +85,6 @@ def stop_play(wrap, cond_filled):
 def recv_thread_func(wrap, cond_filled, sock):
     while True:
         message = sock.recv(RECV_BUFFER)
-        print len(message)
         
         if message == None:
             continue 
